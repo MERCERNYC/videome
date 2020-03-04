51883ff1c2a11ce2deb528bbfb1ed3bab1d82d70
Juliana Fogg <julianafogg@gmail.com>
2020-03-04 18:30:53 -0500

    add axios

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,13 @@
+import axios from 'axios';
+
 const KEY = "AIzaSyDfHFnxePSEE2G_l3YObRs0g6xCsz2wRhY";
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+      part: 'snippet', 
+      type: 'video',
+      maxResults: 5,
+      key: `${KEY}`
+  }
+});

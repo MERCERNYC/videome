56effbef5a2899efc5b6e1d0557dee82e8988fa6
Juliana Fogg <julianafogg@gmail.com>
2020-08-30 11:12:17 -0400

    Updates

---
+++ precache-manifest.289756e1a6d16b5377afffedfb848be2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58a314a8c8325c16bbd12486506a4d50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a664f3d5475f08289580",
+    "url": "/static/css/main.4836ed36.chunk.css"
+  },
+  {
+    "revision": "eed260f6c78dde9c9c2d",
+    "url": "/static/js/2.cb1ac7ec.chunk.js"
+  },
+  {
+    "revision": "59fc29711a22a223e9f602decc11b79f",
+    "url": "/static/js/2.cb1ac7ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a664f3d5475f08289580",
+    "url": "/static/js/main.ce1a7d7b.chunk.js"
+  },
+  {
+    "revision": "124df2e52d5a122c8add",
+    "url": "/static/js/runtime-main.ebfa8a8b.js"
+  }
+]);
